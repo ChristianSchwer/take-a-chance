ea0291471d6726a49d2f67a0cdc9f7cbe6f0f07f
Christian Schwer <christianschwer00@gmail.com>
2021-07-13 06:52:33 +0200

    setup app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <Navbar />
+      </div>
+      <div className="App-body">
+        <h1>Welcome to Take a Chance</h1>
+      </div>
+      <div className="App-footer">
+        <Footer />
+      </div>
     </div>
   );
 }
