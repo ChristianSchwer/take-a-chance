3ca4b404b551a81dc5f7eba9b0fbe2364d7c93ab
Christian Schwer <christianschwer00@gmail.com>
2021-07-27 08:33:17 +0200

    Reworked layout and components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import './App.css';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 
-import Card from './components/Card';
+import Productcard from './components/Cards/ProductCard'
+import Ticketcard from './components/Cards/TicketCard';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
-import ScrollToTop from './ScrollToTop';
 
 function App() {
   return (
     <Router>
-      <ScrollToTop />
       <Navbar />
       {/* <Switch> */}
       <div className="page">
-        <div className="landingpage">
+        <div className="landingpage" id="home">
           <div className="landingpagenormal">
             <h1>Take A Chance</h1>
             <p>Kaufe ein Ticket und mache bei der Verlosung mit.</p><br></br>
@@ -21,31 +20,34 @@ function App() {
             <p>Realistische Gewinnchancen da es nur limitierte Tickets auf ein Product gibt.</p>
           </div>
         </div>
-        <div className="productspage">
+        <div className="productspage" id="productspage">
           <h1>Products</h1>
           <p>Some other fancy words.</p>
           <div className=" cards">
-            <Card name="IPhone10" image="images/img-7.jpg" description="Handy" price="999,99€" available="10/1230" time="Verbleibende Zeit: 0 Tage 12:02:14" />
-            <Card name="Urlaubsreise" image="images/img-2.jpg" description="Urlaub in der Caribik" price="450€" available="600/600" time="Verlosung findet statt" />
-            <Card name="PS5" image="images/img-5.jpg" description="Spielekonsole" price="300€" available="0/499" time="Verbleibende Zeit: 4 Tage 23:12:55" />
+            <Productcard name="IPhone10" image="images/img-7.jpg" description="Handy" price="999,99€" available="10/1230" time="Verbleibende Zeit: 0 Tage 12:02:14" />
+            <Productcard name="Urlaubsreise" image="images/img-2.jpg" description="Urlaub in der Caribik" price="450€" available="600/600" time="Verlosung findet statt" />
+            <Productcard name="PS5" image="images/img-5.jpg" description="Spielekonsole" price="300€" available="0/499" time="Verbleibende Zeit: 4 Tage 23:12:55" />
           </div>
         </div>
-        <div className="tickedspage">
+        <div className="tickedspage" id="tickedspage">
           <h1>Tickets</h1>
           <p>Explain the principe or so. idk?</p>
           <div className="cards">
-            <Card name="1er Pack" image="images/img-4.jpg" description="1 Ticket" price="1,00€" />
-            <Card name="5er Pack" image="images/img-3.jpg" description="5 Tickets" price="4,75€" />
-            <Card name="10er Pack" image="images/img-1.jpg" description="10 Tickets" price="9,50€" />
-            <Card name="25er Pack" image="images/img-6.jpg" description="25 Ticket" price="23,75€" />
-            <Card name="50er Pack" image="images/img-8.jpg" description="50 Tickets" price="47,00€" />
-            <Card name="100er Pack" image="images/img-9.jpg" description="100 Tickets" price="94,00€" />
+            <Ticketcard name="1er Pack" image="images/img-4.jpg" description="1 Ticket" price="1,00€" />
+            <Ticketcard name="5er Pack" image="images/img-3.jpg" description="5 Tickets" price="4,75€" />
+            <Ticketcard name="10er Pack" image="images/img-1.jpg" description="10 Tickets" price="9,50€" />
+            <Ticketcard name="25er Pack" image="images/img-6.jpg" description="25 Ticket" price="23,75€" />
+            <Ticketcard name="50er Pack" image="images/img-8.jpg" description="50 Tickets" price="47,00€" />
+            <Ticketcard name="100er Pack" image="images/img-9.jpg" description="100 Tickets" price="94,00€" />
           </div>
         </div>
-        <div className="aboutuspage">
+        <div className="aboutuspage" id="aboutuspage">
           <div className="aboutuspagenormal">
             <h1>About us</h1>
             <p>Some short words About us.</p>
+            <img className="aboutus-img" alt="aboutus" src="images/img-home.jpg"></img>
+            <img className="aboutus-img" alt="aboutus" src="images/img-4.jpg"></img>
+            <p>Maybe more words?</p>
           </div>
         </div>
       </div>
